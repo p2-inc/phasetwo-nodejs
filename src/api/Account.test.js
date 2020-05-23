10dd9977ef53a6ea043c487a2fbc02e53f1a3992
Soma Holiday <soma@somaholiday.com>
2020-05-23 16:53:48 -0700

    Make Account test more robust

--- src/api/Account.test.js
+++ src/api/Account.test.js
@@ -1,23 +1,53 @@
 import Account from './Account';
 import request from './request';
 
+// Note: 'mock' prefix is necessary for these to be usable inside jest.mock()
+const mockRealmUrl = 'https://realm.url';
+const mockAccountUrl = `${mockRealmUrl}/account/`;
+const mockResponse = {
+  username: 'alice',
+  firstName: 'Alice',
+  lastName: 'Liddel',
+  email: 'alice@keycloak.org',
+  emailVerified: false,
+  attributes: {},
+};
+
+// TODO: move this mock to its own module
 jest.mock('./request', () => ({
   __esModule: true,
-  default: (url, options) => {
-    return Promise.resolve({
-      response: `${url} OK!`,
+  default: jest.fn((url, options) => {
+    if (url === mockAccountUrl) {
+      return Promise.resolve({
+        success: true,
+        status: 200,
+        response: mockResponse,
+      });
+    }
+
+    return Promise.reject({
+      success: false,
+      status: 400,
+      response: `Error!`,
     });
-  },
+  }),
 }));
 
 describe('Account', () => {
-  const realmUrl = 'https://realm.url';
   const token = 'VERY_SECRET_TOKEN';
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json, text/plain, */*',
+    Authorization: `Bearer ${token}`,
+  };
 
-  it('get() makes a request', async () => {
-    const account = new Account(realmUrl);
+  it('get() makes properly formed request', async () => {
+    expect.assertions(2);
+    const account = new Account(mockRealmUrl);
+
     const ret = await account.get(token);
-    console.log(ret);
-    expect(ret).toEqual(`https://realm.url/account/ OK!`);
+    expect(request).toHaveBeenCalledWith(mockAccountUrl, { headers });
+
+    expect(ret).toEqual(mockResponse);
   });
 });
